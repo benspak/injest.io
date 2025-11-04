edd519b669500efa4b761dfce47a6455aa0c03a8
Ben S. <89033552+benspak@users.noreply.github.com>
2025-11-04 15:27:14 -0700

    Cleared

--- packages/api/src/jobs/queue.ts
+++
@@ -1,8 +0,0 @@
-import { Queue } from 'bullmq';
-import Redis from 'ioredis';
-
-const connection = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
-
-export const queue = new Queue('indexing-queue', {
-  connection,
-});
